e8b5135cd6653c11360e2d3ab978b57aeb3f57bf
Grace <grace@MacBook-Pro.local>
2020-01-16 16:29:17 -0700

    done

--- practice.js
+++ practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr){
+  return arr[0];
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){
+return arr[arr.length - 1]
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +52,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper (family){
+  for(let i = 0; i < family.length; i++){
+    alert(family[i]) 
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -62,8 +70,13 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
 
-//Code Here
+// let family = ['buddy']
 
+function reversedLooper(letters){
+  for(let i = letters.length - 1; i >= 0; i--){
+    alert(letters[i])
+  } 
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -79,7 +92,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
 //Code Here
 
-
+function evenFinder (nums){
+  
+  for(let i = 0; i < numbers.length; i++){
+    if (nums[i] % 2 !== 0) nums.splice(i ,1);
+  }
+  console.log(nums);
+  return nums;
+  
+}
 
 
 
